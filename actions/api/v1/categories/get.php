bc63633d649f24c7e96303a14fe60b3e8ed765dd
Hamzah Fauzy <hamzahfauzy@Hamzahs-MacBook-Pro.local>
2023-03-19 01:29:29 +0700

    fix category get

--- actions/api/v1/categories/get.php
+++ actions/api/v1/categories/get.php
@@ -11,7 +11,7 @@ $conn  = conn();
 $db    = new Database($conn);
 $user  = ApiUser::getUser();
 
-$db->query = "SELECT * FROM categories WHERE id NOT IN (SELECT category_id FROM propretaries WHERE user_id = $user->id AND expired_at > ".date('Y-m-d H:i:s').")";
+$db->query = "SELECT * FROM categories WHERE id NOT IN (SELECT category_id FROM proprietaries WHERE user_id = $user->id AND expired_at > ".date('Y-m-d H:i:s').")";
 $data = $db->exec('all');
 
 echo json_encode([
